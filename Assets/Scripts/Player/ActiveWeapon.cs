274b26f7eb302a3e917a46a4214984088ea74105
AnvarTrf <noora.trf27@gmail.com>
2024-05-18 02:58:19 +0330

    Weapon Cooldown + Bow + Arrow

--- Assets/Scripts/Player/ActiveWeapon.cs
+++ Assets/Scripts/Player/ActiveWeapon.cs
@@ -8,6 +8,7 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
     public MonoBehaviour CurrentActiveWeapon { get; private set; }
 
     private PlayerControls playerControls;
+    private float timeBetweenAttacks;
 
     private bool attackButtonDown, isAttacking = false;
 
@@ -26,6 +27,8 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
     {
         playerControls.Combat.Attack.started += _ => StartAttacking();
         playerControls.Combat.Attack.canceled += _ => StopAttacking();
+
+        AttackCooldown();
     }
 
     private void Update() {
@@ -34,14 +37,24 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
 
     public void NewWeapon(MonoBehaviour newWeapon) {
         CurrentActiveWeapon = newWeapon;
+
+        AttackCooldown();
+        timeBetweenAttacks = (CurrentActiveWeapon as IWeapon).GetWeaponInfo().weaponCooldown;
     }
 
     public void WeaponNull() {
         CurrentActiveWeapon = null;
     }
 
-    public void ToggleIsAttacking(bool value) {
-        isAttacking = value;
+    private void AttackCooldown() {
+        isAttacking = true;
+        StopAllCoroutines();
+        StartCoroutine(TimeBetweenAttacksRoutine());
+    }
+
+    private IEnumerator TimeBetweenAttacksRoutine() {
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        isAttacking = false;
     }
     
     private void StartAttacking()
@@ -56,7 +69,7 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
 
     private void Attack() {
         if (attackButtonDown && !isAttacking) {
-            isAttacking = true;
+            AttackCooldown();
             (CurrentActiveWeapon as IWeapon).Attack();
         }
     }
